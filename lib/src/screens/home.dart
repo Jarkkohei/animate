2e7df159e2842fc1242a6c3536e64935695560be
Jarkko <jarkkohei@luukku.com>
2019-05-03 18:32:33 +0300

    Initialize catController and catAnimation

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -6,7 +6,27 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with TickerProviderStateMixin {
+
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  initState() {
+    super.initState();
+
+    catController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+
+    catAnimation = Tween(begin: 0.0, end: 100.0)
+      .animate(
+        CurvedAnimation(
+          parent: catController,
+          curve: Curves.easeIn,
+        ),
+      );
+  }
 
   Widget buildAnimation() {
     return Cat();
